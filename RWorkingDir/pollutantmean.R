7bca47201676db102cae5083d61ef347cef98ece
S. Jefferson Davis IV <SJDavisIV@comcast.net>
2014-09-10 16:16:52 -0400

    updated file

--- RWorkingDir/pollutantmean.R
+++ RWorkingDir/pollutantmean.R
@@ -12,5 +12,16 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 
-  
+
+    
+files_list <- list.files(directory, full.names=TRUE) ## Pull all files from dir
+
+p_data <- data.frame()
+
+for (i in id) {
+        p_data <- rbind(p_data, read.csv(files_list[i])) 
+    }
+  eval_data <- p_data[pollutant]
+  bad <- is.na(eval_data)   
+  mean(eval_data[!bad])
 }
