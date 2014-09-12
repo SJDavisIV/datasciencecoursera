39efb534141ff5ae08785bc46e823f243c0ea5b5
S. Jefferson Davis IV <SJDavisIV@comcast.net>
2014-09-12 15:50:24 -0400

    updated

--- RWorkingDir/corr.R
+++ RWorkingDir/corr.R
@@ -6,19 +6,42 @@ corr <- function(directory, threshold = 0) {
     ## number of completely observed observations (on all
     ## variables) required to compute the correlation between
     ## nitrate and sulfate; the default is 0
-    
+
     ## Return a numeric vector of correlations
 
 
 
 files_list <- list.files(directory, full.names=TRUE) ## Pull all files from dir
 
-size <- length(files_list)
+id <- 1:332
+
+source("complete.R")
+
+t_obs <- subset(complete(directory), nobs > threshold) 
+    ## All complete oberservations within the threshold 
+
+t_mon <- t_obs$id
+    ## Vector of monitors with complete observation
 
 p_data <- data.frame()
 
-for (i in 1:size) {
-          p_data <- rbind(p_data, read.csv(files_list[i])) 
+for (i in id) {
+    p_data <- rbind(p_data, read.csv(files_list[i]))
+    
 }
+
+size <- length(t_mon) ## Iterations
+
+mon_cor <- data.frame()  ## stores each monitor
+
+c <- vector("numeric") ## stores return value
+if(size > 0) {
+for (i in 1:size) {
+        mon_cor <- subset(p_data, ID == t_mon[i])
+        c[i] <- cor(mon_cor$nitrate, mon_cor$sulfate, use = "complete.obs")        
+}
+}
+
+c
 
 }
